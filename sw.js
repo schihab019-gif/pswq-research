97c686fab2535a2e4c984a84ddc9d04c7ae36181
schihab019-gif <schihab019@gmail.com>
2026-04-12 22:13:22 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,16 @@
-self.addEventListener('fetch', () => {});
+const cacheName = 'pswq-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', async el => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+});
+
+self.addEventListener('fetch', el => {
+  el.respondWith(fetch(el.request));
+});
